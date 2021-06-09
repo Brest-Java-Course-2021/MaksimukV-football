fb7afe17e524af905f021180fcc5f8e6988c9623
Uladzislau Maksimuk <uladzislau_maksimuk@epam.com>
2021-06-09 11:36:43 +0300

    Project presentation

---
+++ db/target/classes/init-db.sql
@@ -0,0 +1,7 @@
+INSERT INTO TEAM (TEAM_ID, TEAM_NAME) VALUES ( 1, 'Arizona Cardinals' );
+INSERT INTO TEAM (TEAM_ID, TEAM_NAME) VALUES ( 2, 'Atlanta Falcons' );
+
+INSERT INTO PLAYER (firstname, lastname, nationality, salary, team_id) VALUES ('Maxx', 'Williams', 'American', 10000, 1);
+INSERT INTO PLAYER (firstname, lastname, nationality, salary, team_id) VALUES ('Charles', 'Washington', 'Brazilian', 12000, 1);
+INSERT INTO PLAYER (firstname, lastname, nationality, salary, team_id) VALUES ('Sena', 'Green', 'Brazilian', 10000, 1);
+INSERT INTO PLAYER (firstname, lastname, nationality, salary, team_id) VALUES ('John', 'Wetzel', 'American', 9000, 2);
